441f8c2f3fc6ccfb9f24e1e161ee332e13f02774
Ward <award40@LAMU0CLP74YXVX6.uhc.com>
2023-11-01 15:47:15 +0000

    aider: Added /Users/award40/Desktop/personal/github/generative-ai-workbook/learning/autogen/examples/postgres_ds/main.py

--- learning/autogen/examples/postgres_ds/main.py
+++ learning/autogen/examples/postgres_ds/main.py
@@ -8,27 +8,30 @@ import dotenv
 import autogen
 import argparse
 
-# sys.path.append('./')
+# sys.path.append('./') 
 dotenv.load_dotenv()
 
-# from .agents import *
+from agents import ad #(safe_get, response_parser, prompt, add_cap_ref)  
 # from .api import *
 # from .config import *
 # from .data.dictionaries import *
+
 from data.db import PostgresManager
+from utils.agent_utils import (get_config)
 
 
 DB_URL = os.environ.get("POSTGRES_CONNECTION_URL")
 OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
 def main():
-    # parser = argparse.ArgumentParser()
-    # parser.add_argument("prompt", help="The prompt for the AI")
-    # args = parser.parse_args()
-
-    # config_list = get_config(model_type='gpt-4', dotenv_path='../../../.env')
-
     # parse prompt param using arg parse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("prompt", help="The prompt for the AI")
+    args = parser.parse_args()
+
+    # get autogen config list
+    config_list = get_config(model_type='gpt-4', dotenv_path='../../../.env')
+
     # connect to db using with statement and create a db_manager
     with PostgresManager() as db:
         db.connect_with_url(DB_URL)
@@ -36,17 +39,16 @@ def main():
         users_table = db.get_all(table_name="patients")
         print("users_table", users_table)
 
+    # call db_manager.get_table_definition_for_prompt () to get tables in prompt ready form
 
-        # parse prompt param using arg parse
-        # connect to b using with statement and create a db_manager
-        # call db_manager.get_table_definition_for_prompt () to get tables in prompt ready form
-        # create two blank calls to 1lm.add_cap_ref() that update our current prompt passed in from cli
-        # call llm.prompt to get a prompt_ response variable
-        # parse sql response from prompt_response using SQL_QUERY_DELIMITER '---------'
-        # call db_manager. run_sql() with the parsed sql
-        pass
+    # create two blank calls to 1lm.add_cap_ref() that update our current prompt passed in from cli
 
+    # call llm.prompt to get a prompt_ response variable
 
+    # parse sql response from prompt_response using SQL_QUERY_DELIMITER '---------'
+
+    # call db_manager. run_sql() with the parsed sql
+    pass
 
 if __name__ == "__main__":
     main()

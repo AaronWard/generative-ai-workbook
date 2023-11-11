82a35a6e0cdd163f9b911e0b480fd8563ceb58f3
Ward <award40@LAMU0CLP74YXVX6.uhc.com>
2023-11-11 16:28:03 +0000

    MOVE - functions from app.py to ui_utils.py

--- learning/autogen/examples/postgres_ds/app.py
+++ learning/autogen/examples/postgres_ds/app.py
@@ -18,13 +18,27 @@ import plotly.graph_objects as go
 from dotenv import find_dotenv, load_dotenv
 from agents.db_agent import DBAgent
 
+from utils.ui_utils import (save_logs, setup_chat_settings, setup_avatars, 
+                            handle_message_indentation, update_cost_counter, 
+                            send_final_response, handle_text_file, 
+                            handle_image_file, setup_agents, get_response)
+
 output_folder = "_output/"
 logs_filename = f"{output_folder}_logs/conversations_{datetime.now().timestamp()}.json"
+naming_dict = {
+    "User_Proxy": "User Proxy",
+    "assistant": "Assistant Agent",
+    "User Proxy": "User Proxy",
+    "User": "User",
+    "user": "User Proxy",
+    "function": "Code output",
+    "Data_Engineer": "Data Engineer"
+}
+
 
 # PROBLEM_TYPE = "COMPLEX"
 PROBLEM_TYPE = "SIMPLE"
 TOTAL_COST = 0.0
-MAX_ITER = 100
 USER_NAME = "User"
 USER_PROXY_NAME = "User Proxy"
 ASSISTANT_NAME = "Data Engineer"
@@ -39,117 +53,16 @@ load_dotenv(find_dotenv())
 
 ############################ Agent Functions #####################################
 
-async def setup_agents(temperature: float,
-                        model: str):
-    """Create Agent objects and set user session variables"""
-    agent = DBAgent(
-        work_dir=output_folder,
-        temperature=temperature,
-        model=model
-    )
-    agent.clear_history(clear_previous_work=True)
-
-    # Setup subagent interactions
-    agent.instantiate_groupchat()
-    agent.instantiate_two_way_chat()
-
-    # User Session Variables TODO: make this dynamic
-    two_way_secondary_agent_name = agent.two_way_secondary_agent.name.replace("_", " ")
-    two_way_user_proxy_name = agent.two_way_user_proxy.name.replace("_", " ")
-
-    # Setting user session variables
-    cl.user_session.set('agent', agent)
-    cl.user_session.set(two_way_secondary_agent_name, agent.two_way_secondary_agent)
-    cl.user_session.set(two_way_user_proxy_name, agent.two_way_user_proxy)    
-
-    # groupchat_secondary_agent_name = agent.groupchat_secondary_agent.name.replace("_", " ") # name
-    # groupchat_user_proxy_name = agent.groupchat_user_proxy.name.replace("_", " ")  # admin_name
-    # cl.user_session.set(groupchat_secondary_agent_name, agent.groupchat_secondary_agent)
-    # cl.user_session.set(groupchat_user_proxy_name, agent.groupchat_user_proxy)
-
-async def save_logs(logs_filename=logs_filename):
-    # Make sure the directory exists
-    os.makedirs(os.path.dirname(logs_filename), exist_ok=True)
-
-    # Now save the logs
-    logs = autogen.ChatCompletion.logged_history
-    with open(logs_filename, "w") as log_file:
-        json.dump(logs, log_file, indent=4)
-
-    return logs
-
 @cl.on_settings_update
 async def update_agent_settings(settings):
     """Used to update agent"""
     await setup_agents(temperature=settings['Temperature'],
-                        model=settings['Model'])
+                        model=settings['Model'], 
+                        output_folder=output_folder)
 
     print(f"Settings updated {settings}")
 
-
 ########################## User Interface Functions ########################################
-
-naming_dict = {
-    "User_Proxy": "User Proxy",
-    "assistant": "Assistant Agent",
-    "User Proxy": "User Proxy",
-    "User": "User",
-    "user": "User Proxy",
-    "function": "Code output",
-    "Data_Engineer": "Data Engineer"
-}
-
-
-async def setup_avatars():
-    """Function for avatar icons"""
-    avatar_configurations = [
-        {
-            "name": "User",
-            "url": "https://api.dicebear.com/7.x/thumbs/svg?seed=Callie&rotate=360&eyes=variant4W14&eyesColor=ffffff,000000",
-        },
-        {
-            "name": "User Proxy",
-            "url": "https://api.dicebear.com/7.x/bottts-neutral/svg?seed=Fluffy&radius=45&backgroundColor=546e7a",
-        },
-        {
-            "name": "Data Engineer",
-            "url": "https://api.dicebear.com/7.x/bottts-neutral/svg?seed=Fluffy&radius=45&backgroundColor=546e7a",
-        },
-        {
-            "name": "chatbot",
-            "url": "https://api.dicebear.com/7.x/bottts-neutral/svg?seed=Dusty&backgroundColor=ffb300",
-        },
-        {
-            "name": "Code output",
-            "url": "https://api.dicebear.com/7.x/bottts-neutral/svg?seed=Fluffy&radius=45&backgroundColor=546e7a",
-        },
-        {
-            "name": "Assistant Agent",
-            "url": "https://api.dicebear.com/7.x/bottts-neutral/svg?seed=Fluffy&radius=45&backgroundColor=546e7a",
-        },
-    ]
-    
-    for avatar in avatar_configurations:
-        await cl.Avatar(
-            name=avatar["name"],
-            url=avatar["url"],
-        ).send()
-
-async def setup_chat_settings():
-    # Set up agent configuration
-    settings = await cl.ChatSettings(
-            [
-                cl.input_widget.Select(
-                    id="Model",
-                    label="Model",
-                    values=["gpt-3.5-turbo", "gpt-3.5-turbo-16k", "gpt-4", "gpt-4-32k"],
-                    initial_index=2
-                ),
-                cl.input_widget.Slider(id="Temperature", label="Temperature (randomness)", initial=0.5, min=0, max=2, step=0.1),
-            ]
-    ).send()
-
-    return settings
 
 @cl.action_callback("on_chat_start_action")
 async def on_action(action):
@@ -196,114 +109,14 @@ async def setup_chat():
     # Initialize Agents
     settings = await setup_chat_settings()
     await setup_agents(temperature=settings['Temperature'],
-                        model=settings['Model'])
+                        model=settings['Model'],
+                        output_folder=output_folder)
 
     cl.user_session.set("total_cost", TOTAL_COST)
     
 
 ########################## Message Handling Functions ########################################
 
-async def handle_text_file(file_element):
-    # Process text file
-    pass
-
-async def handle_image_file(file_element):
-    # Process image file
-    pass
-
-
-async def get_response(user_message: str):
-    agent = cl.user_session.get("agent")
-    assistant = cl.user_session.get(agent.two_way_secondary_agent.name.replace("_", " "))
-    user_proxy = cl.user_session.get(agent.two_way_user_proxy.name.replace("_", " "))
-
-    # Run the agent to get initial or continued response
-    # TODO: Stream responses to UI
-    if len(assistant.chat_messages[user_proxy]) == 0:
-        print('initiating a conversation with agent.run()')
-        await cl.make_async(agent.run)(problem_type=PROBLEM_TYPE, prompt=user_message)
-    else:
-        print('continuing a conversation with agent._continue()')
-        await cl.make_async(agent._continue)(problem_type=PROBLEM_TYPE, prompt=user_message)
-
-async def handle_message_indentation():
-    agent = cl.user_session.get("agent")
-    assistant = cl.user_session.get(agent.two_way_secondary_agent.name.replace("_", " "))
-    user_proxy = cl.user_session.get(agent.two_way_user_proxy.name.replace("_", " "))
-    final_response = None
-
-    # Retrieve and filter message history
-    original_message_history = assistant.chat_messages[user_proxy]
-    last_seen_message_index = cl.user_session.get('last_seen_message_index', 0)
-    new_message_history = original_message_history[last_seen_message_index:]
-
-
-    # Process messages and check for TERMINATE condition
-    for i, message in enumerate(new_message_history):
-        content = message.get("content", "").strip() if message.get("content") else ""
-        function_call = message.get("function_call", "") if message.get("function_call") else ""
-
-        # Check for TERMINATE condition
-        if "TERMINATE" in content:
-            # If TERMINATE is the exact content of the last message, disregard it
-            if content == "TERMINATE" and i == len(new_message_history) - 1:
-                if i > 0:  # Ensure there is a message before the TERMINATE message
-                    final_response = new_message_history[i - 1].get("content", "").strip()
-                break  # Stop processing as we've reached the end
-            else:
-                content = content.strip().replace("TERMINATE.", "").replace("TERMINATE", "").strip()  # Strip TERMINATE from the content
-
-        # Only send non-empty and non-TERMINATE contents
-        if content:
-            await cl.Message(
-                author=naming_dict[message["role"]],
-                content=content,
-                indent=1
-            ).send()
-        if function_call:
-            await cl.Message(
-                author=naming_dict[message["role"]],
-                content=function_call,
-                indent=1
-            ).send()
-
-    cl.user_session.set('last_seen_message_index', len(original_message_history))
-    if final_response is not None:
-        final_response = final_response.strip().replace("TERMINATE.", "").replace("TERMINATE", "")
-    print(final_response)
-    return final_response
-
-
-async def send_final_response(final_response):
-            # Send the final message
-    if final_response:
-        await cl.Message(
-            author="chatbot",
-            content=final_response,
-            indent=0
-        ).send()
-
-        chart_fig = None  # Get the chart figure
-        if chart_fig:
-            pass # TODO: Make code for displaying plotly dynamically in UI
-    else:
-        await cl.Message(
-            author="chatbot",
-            content="Sorry, please try again. we got lost in thought...",
-            indent=0
-        ).send()
-
-def update_cost_counter(logs):
-    # Display cost logs and update total cost
-    conversation = next(iter(logs.values()))
-    cost = sum(conversation["cost"])
-    TOTAL_COST = cl.user_session.get("total_cost", 0.0) + cost
-    cl.user_session.set("total_cost", TOTAL_COST)
-
-    # Display cost counter
-    cost_counter = cl.TaskList(status="Done")
-    cost_task = cl.Task(title=f"Total Cost in USD for this conversation: ${TOTAL_COST:.2f}", status=cl.TaskStatus.DONE)
-    return cost_counter, cost_task
 
 @cl.on_message
 async def handle_message(user_message: dict):
@@ -335,18 +148,19 @@ async def handle_message(user_message: dict):
         print(f"Start logging...")
 
         # Ensure the get_response is awaited properly
-        await get_response(user_message.content)
+        await get_response(problem_type=PROBLEM_TYPE, 
+                           user_message=user_message.content)
         print(f"Make call to get_response...")
 
         # Ensure the save_logs is awaited properly
         print(f"Make call to save_logs...")
-        logs = await save_logs()
+        logs = await save_logs(logs_filename=logs_filename)
 
         autogen.ChatCompletion.stop_logging()
         print(f"stopped logging...")
 
         # Process and display messages
-        final_response = await handle_message_indentation()
+        final_response = await handle_message_indentation(naming_dict)
         print(f"Got final response: {final_response}")
 
         # Send final response if available
--- learning/autogen/examples/postgres_ds/utils/agent_utils.py
+++ learning/autogen/examples/postgres_ds/utils/agent_utils.py
@@ -1,2 +1,3 @@
+import os
+import json
 import autogen
-    
